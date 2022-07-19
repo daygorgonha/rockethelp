eaa39f0a1cda44541571a2ab3199fd7cc25c87d8
Dayane Gorgonha <dayanebismark18@gmail.com>
2022-07-18 23:50:53 -0300

    feat: creation of login screen and components

--- App.tsx
+++ App.tsx
@@ -1,6 +1,19 @@
 import { SignIn } from "./src/screens/Signin";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import {useFonts,Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import { THEME } from "./src/styles/theme";
+import { Loading } from "./src/components/Loading";
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
   return (
-    <SignIn />
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <SignIn /> : <Loading/>}
+    </NativeBaseProvider>
   );
 }
