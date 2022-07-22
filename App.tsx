a8a77c7c29532d7b9a087f3f95f78d6720d80773
Dayane Gorgonha <dayanebismark18@gmail.com>
2022-07-21 21:48:47 -0300

    feat: implementing authentication

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { SignIn } from "./src/screens/Signin";
+import { SignIn } from "./src/screens/SignIn";
 import { NativeBaseProvider, StatusBar } from "native-base";
 import {useFonts,Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import { THEME } from "./src/styles/theme";
